e37368daecfe67f0e9815c537e7993ef1bf4af90
Keiroru <124210421+Keiroru@users.noreply.github.com>
2024-11-19 23:39:27 +0100

    Add files via upload

--- Color-Match/asmyn.js
+++ Color-Match/asmyn.js
@@ -1,11 +1,51 @@
 const colors = ["Aqua", "Aquamarine", "Beige", "Black", "Blue", "BlueViolet", "Brown", "Chartreuse", "Crimson", "DarkBlue", "DarkOrange", "DeepPink",];
 
 var len = colors.length;
+var data1, data2, data3;
+var score = 0;
 
 function start() {
-    writeColor();
+    meaningText();
+    textColor();
 }
-function writeColor() {
-    var asd = Math.floor(Math.random() * len);
-    document.getElementById("colorText").innerHTML = colors[asd];
+function meaningText() {
+    var random = Math.floor(Math.random() * len);
+    data1 = random;
+    document.getElementById("meaningText").innerHTML = colors[random];
+}
+
+function textColor() {
+    var random = Math.floor(Math.random() * len);
+    var random1 = Math.floor(Math.random() * len);
+    data2 = random;
+    data3 = random1;
+    document.getElementById("colorText").innerHTML = colors[random];
+    document.getElementById("colorText").style.color = colors[random1];
+}
+
+document.addEventListener('keydown', function (pressedKey) {
+    if (pressedKey.key == "ArrowRight") {
+        answer(true);
+    }
+    else if (pressedKey.key == "ArrowLeft") {
+        answer(false);
+    }
+})
+
+function answer(ans) {
+    if (ans == true && data1 == data3) {
+        score += 1;
+        console.log(score);
+        start();
+    }
+    else if (ans == false && data1 != data3) {
+        score += 1;
+        console.log(score);
+        start();
+    }
+    else {
+        console.log('szar vagy hihi ;)')
+        start();
+    }
+
 }
